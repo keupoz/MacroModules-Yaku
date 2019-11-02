a740dc093c4d87bfe668d22f1628f91dc270e72e
keupoz <keupoz@gmail.com>
2019-11-02 20:03:06 +0700

    Added ackermann action

---
+++ src/main/java/ru/keupoz/macromodules/yaku/actions/ScriptActionAckermann.java
@@ -0,0 +1,55 @@
+package ru.keupoz.macromodules.yaku.actions;
+
+import net.eq2online.macros.scripting.api.APIVersion;
+import net.eq2online.macros.scripting.api.IMacro;
+import net.eq2online.macros.scripting.api.IMacroAction;
+import net.eq2online.macros.scripting.api.IReturnValue;
+import net.eq2online.macros.scripting.api.IScriptActionProvider;
+import net.eq2online.macros.scripting.api.ReturnValue;
+import net.eq2online.macros.scripting.exceptions.ScriptExceptionStackOverflow;
+import net.eq2online.macros.scripting.parser.ScriptAction;
+import net.eq2online.macros.scripting.parser.ScriptContext;
+import net.eq2online.macros.scripting.parser.ScriptCore;
+import ru.keupoz.macromodules.yaku.ModuleInfo;
+
+@APIVersion(ModuleInfo.API_VERSION)
+public class ScriptActionAckermann extends ScriptAction {
+    public ScriptActionAckermann() {
+        this(ScriptContext.MAIN);
+    }
+
+    public ScriptActionAckermann(ScriptContext context) {
+        super(context, "ackermann");
+    }
+
+    private int calculate(int m, int n) {
+        if (m == 0) {
+            return n + 1;
+        } else if (m > 0 && n == 0) {
+            return this.calculate(m - 1, 1);
+        } else if (m > 0 && n > 0) {
+            return this.calculate(m - 1, this.calculate(m, n - 1));
+        }
+
+        return 0;
+    }
+
+    public IReturnValue execute(IScriptActionProvider provider, IMacro macro, IMacroAction instance, String rawParams, String[] params) {
+        int m = ScriptCore.tryParseInt(provider.expand(macro, params[0], false), 0);
+        int n = ScriptCore.tryParseInt(provider.expand(macro, params[1], false), 0);
+
+        int result = 0;
+
+        try {
+            result = this.calculate(m, n);
+        } catch (StackOverflowError err) {
+            throw new ScriptExceptionStackOverflow();
+        }
+
+        return new ReturnValue(result);
+    }
+
+    public void onInit() {
+        this.context.getCore().registerScriptAction(this);
+    }
+}
